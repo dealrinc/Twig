ae8084c0d8e1e90dab7fbb0f4253f4047800286a
Forrest Middleton <forrest@insightautomotive.com>
2022-10-25 15:40:59 -0600

    Restore the for loop if statement

--- src/TokenParser/ForTokenParser.php
+++ src/TokenParser/ForTokenParser.php
@@ -12,10 +12,15 @@
 
 namespace Twig\TokenParser;
 
+use Twig\Error\SyntaxError;
 use Twig\Node\Expression\AssignNameExpression;
+use Twig\Node\Expression\ConstantExpression;
+use Twig\Node\Expression\GetAttrExpression;
+use Twig\Node\Expression\NameExpression;
 use Twig\Node\ForNode;
 use Twig\Node\Node;
 use Twig\Token;
+use Twig\TokenStream;
 
 /**
  * Loops over each item of a sequence.
@@ -25,8 +30,6 @@ use Twig\Token;
  *      <li>{{ user.username|e }}</li>
  *    {% endfor %}
  *   </ul>
- *
- * @internal
  */
 final class ForTokenParser extends AbstractTokenParser
 {
@@ -37,6 +40,13 @@ final class ForTokenParser extends AbstractTokenParser
         $targets = $this->parser->getExpressionParser()->parseAssignmentExpression();
         $stream->expect(/* Token::OPERATOR_TYPE */ 8, 'in');
         $seq = $this->parser->getExpressionParser()->parseExpression();
+
+        $ifexpr = null;
+        if ($stream->nextIf(/* Token::NAME_TYPE */ 5, 'if')) {
+            // @trigger_error(sprintf('Using an "if" condition on "for" tag is deprecated since Twig 2.10.0, use a "filter" filter or an "if" condition inside the "for" body instead (if your condition depends on a variable updated inside the loop).', __CLASS__), E_USER_DEPRECATED);
+
+            $ifexpr = $this->parser->getExpressionParser()->parseExpression();
+        }
 
         $stream->expect(/* Token::BLOCK_END_TYPE */ 3);
         $body = $this->parser->subparse([$this, 'decideForFork']);
@@ -52,13 +62,19 @@ final class ForTokenParser extends AbstractTokenParser
             $keyTarget = $targets->getNode(0);
             $keyTarget = new AssignNameExpression($keyTarget->getAttribute('name'), $keyTarget->getTemplateLine());
             $valueTarget = $targets->getNode(1);
+            $valueTarget = new AssignNameExpression($valueTarget->getAttribute('name'), $valueTarget->getTemplateLine());
         } else {
             $keyTarget = new AssignNameExpression('_key', $lineno);
             $valueTarget = $targets->getNode(0);
+            $valueTarget = new AssignNameExpression($valueTarget->getAttribute('name'), $valueTarget->getTemplateLine());
         }
-        $valueTarget = new AssignNameExpression($valueTarget->getAttribute('name'), $valueTarget->getTemplateLine());
 
-        return new ForNode($keyTarget, $valueTarget, $seq, null, $body, $else, $lineno, $this->getTag());
+        if ($ifexpr) {
+            $this->checkLoopUsageCondition($stream, $ifexpr);
+            $this->checkLoopUsageBody($stream, $body);
+        }
+
+        return new ForNode($keyTarget, $valueTarget, $seq, $ifexpr, $body, $else, $lineno, $this->getTag());
     }
 
     public function decideForFork(Token $token): bool
@@ -69,6 +85,47 @@ final class ForTokenParser extends AbstractTokenParser
     public function decideForEnd(Token $token): bool
     {
         return $token->test('endfor');
+    }
+
+    // the loop variable cannot be used in the condition
+    private function checkLoopUsageCondition(TokenStream $stream, Node $node): void
+    {
+        if ($node instanceof GetAttrExpression && $node->getNode('node') instanceof NameExpression && 'loop' == $node->getNode('node')->getAttribute('name')) {
+            throw new SyntaxError('The "loop" variable cannot be used in a looping condition.', $node->getTemplateLine(), $stream->getSourceContext());
+        }
+
+        foreach ($node as $n) {
+            if (!$n) {
+                continue;
+            }
+
+            $this->checkLoopUsageCondition($stream, $n);
+        }
+    }
+
+    // check usage of non-defined loop-items
+    // it does not catch all problems (for instance when a for is included into another or when the variable is used in an include)
+    private function checkLoopUsageBody(TokenStream $stream, Node $node): void
+    {
+        if ($node instanceof GetAttrExpression && $node->getNode('node') instanceof NameExpression && 'loop' == $node->getNode('node')->getAttribute('name')) {
+            $attribute = $node->getNode('attribute');
+            if ($attribute instanceof ConstantExpression && \in_array($attribute->getAttribute('value'), ['length', 'revindex0', 'revindex', 'last'])) {
+                throw new SyntaxError(sprintf('The "loop.%s" variable is not defined when looping with a condition.', $attribute->getAttribute('value')), $node->getTemplateLine(), $stream->getSourceContext());
+            }
+        }
+
+        // should check for parent.loop.XXX usage
+        if ($node instanceof ForNode) {
+            return;
+        }
+
+        foreach ($node as $n) {
+            if (!$n) {
+                continue;
+            }
+
+            $this->checkLoopUsageBody($stream, $n);
+        }
     }
 
     public function getTag(): string
